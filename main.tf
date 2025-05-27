9f27752e7cbd518e9ba6ba85a95510a6d73c4756
Random._. <rishangbhavsarcs@gmail.com>
2025-05-27 13:19:55 +0000

    fix: ecr url

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ locals {
     digit = "/\"(-[[:digit:]]|[[:digit:]]+)\"/"
   }
 
-  image = var.ecr_create_repo ? "${aws_ecr_repository.service[0].name}:${var.ecr_task_definition_tag}" : var.image
+  image = var.ecr_create_repo ? "${aws_ecr_repository.service[0].repository_url}:${var.ecr_task_definition_tag}" : var.image
 
   template_file = templatefile(
     "${path.module}/templates/container-definition.json.tpl",
